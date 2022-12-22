3214a80cc0cbd1ff52ee07a16c8a9473abdbe3dd
Arvil Meña <arvilmena@gmail.com>
2022-12-22 08:29:26 +0800

    custom local fonts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,20 @@
-import React from "react";
-export default function Layout({ children }: React.PropsWithChildren) {
-    return <div className="container mx-auto">{children}</div>;
-}
+import React, { PropsWithChildren } from "react";
+type LayoutProps = PropsWithChildren<{ className?: string | undefined }>;
+// export default function Layout({ className = "" }: PropsWithChildren) {
+//     return <div className="container mx-auto">{children}</div>;
+// }
+
+export const Layout: React.FC<PropsWithChildren<LayoutProps>> = ({
+    className = "",
+    ...props
+}) => {
+    return (
+        <>
+            <div className={`container mx-auto ${className ?? ""}`}>
+                {props.children}
+            </div>
+        </>
+    );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 import Layout from "@/components/Layout";
+
+const alaskaFont = localFont({
+    src: [
+        {
+            path: "./../fonts/Alaska Trial WOFF2/AlasTrial-Regular.woff2",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "./../fonts/Alaska Trial WOFF2/AlasTrial-Medium.woff2",
+            weight: "500",
+            style: "normal",
+        },
+    ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
+        <Layout className={alaskaFont.className}>
             <Component {...pageProps} />
         </Layout>
     );
