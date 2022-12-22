8642b65c4bf2ffd5319eff9e5af9f65130a483e4
Arvil Meña <arvilmena@gmail.com>
2022-12-22 08:36:35 +0800

    components as barrel export

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from "@next/font/local";
-import Layout from "@/components/Layout";
+import { Layout } from "@/components";
 
 const alaskaFont = localFont({
     src: [
